6e410bd09f27c47dd31565797861db5b2e74cc63
Teagan Horkan <tgnhrkn@gmail.com>
2019-11-21 18:19:09 -0800

    light_board almost ready

--- src/light_board.py
+++ src/light_board.py
@@ -2,42 +2,19 @@ from fpioa_manager import *
 from Maix import FPIOA, GPIO
 from machine import UART
 from board import board_info
+from modes import *
+from Led import *
 
-buttons = {}
 bt_uart = None
+cv_uart = None
+lstrip = None
+n_modes = 3
 
 #Main code file for the LED driving board
 # Tasks:
 # 1) check buttons, BT, ML board for data
 # 2) Make decision on mode transfer
 # 3) call mode update
-
-
-# For now, three buttons:
-# next, previous, and on/off
-#
-# next: io6
-# prev: io7
-# 1/0 : io8
-#
-def init_buttons():
-    next_pin = 6
-    prev_pin = 7
-    onoff_pin = 8
-    fpioa.set_function( next_pin, FPIOA.GPIO6 )
-    fpioa.set_function( prev_pin, FPIOA.GPIO7 )
-    fpioa.set_function( onoff_pin, FPIOA.GPIO8 )
-    next_gpio = GPIO( GPIO.GPIO6, GPIO.IN )
-    prev_gpio = GPIO( GPIO.GPIO7, GPIO.IN )
-    onoff_gpio = GPIO( GPIO.GPIO8, GPIO.IN )
-    return { 'next': next_gpio, 'prev': prev_gpio, 'onoff': onoff_gpio }
-
-def read_buttons():
-    reads = {}
-    for fcn, gpio in buttons:
-        reads[fcn] = gpio.value()
-    return reads
-
 
 # bt uart will run off pins 9(RX) and 10(TX)
 def init_bt_uart():
@@ -49,58 +26,74 @@ def init_bt_uart():
     fm.register( board_info.PIN9, fm.fpioa.UART2_RX, force=True )
     return UART( UART.UART2, baud, bits, 0, 0, timeout=timeout, read_buf_len=buf_len )
 
-
 # returns None if there is no data in the buffer
 # otherwise returns any onoff seen
 # otherwise returns the last thing recv'd
 def read_bt_uart():
-    recv = uart.read()
-    if res is None:
-        return res
-    if bytes('0') in recv:
-        return bytes('0')
-    return recv[-1]
+    recv = bt_uart.read()
+    if recv is None:
+        return None
+    
+    try:
+        recv = recv.decode()
+        return int( recv[-1] )
+    except:
+        return None
 
-# run off pins 21 (RX) and 22 (TX)
+# run off pins 17 (RX) and 15 (TX)
 def init_cv_uart():
-    fm.register( board_info.PIN21, fm.fpioa.UART1_RX, force=True )
-    fm.register( board_info.PIN22, fm.fpioa.UART1_TX, force=True )
+    fm.register( board_info.PIN17, fm.fpioa.UART1_RX, force=True )
+    fm.register( board_info.PIN15, fm.fpioa.UART1_TX, force=True )
     return UART( UART.UART1, 9600, 8, 0, 0, timeout=10, read_buf_len=1024 )
 
 def read_cv_uart():
-    recv = uart.read()
-    if res is None:
-        return res
-    if bytes('0') in recv:
-        return bytes('0')
+    recv = cv_uart.read()
+    if recv is None:
+        return None
+    if b'0' in recv:
+        return b'0'
     return recv[-1]
 
 def init_leds():
-    return False
+    global lstrip
+    lstrip = LedStrip(6, 9)
 
 def init_hw():
-    init_buttons()
-    init_bt_uart()
-    init_cv_uart()
+    global bt_uart, cv_uart
+    bt_uart = init_bt_uart()
+    cv_uart = init_cv_uart()
+    init_leds()
 
 def init_modes():
-    modes = []
-    modes.append( Mode() )
+    modes = list( [ Mode(lstrip) for _ in range(n_modes) ] )
+    modes[0] = OffMode(lstrip) 
+    modes[1] = FlashMode(lstrip) 
+    modes[2] = MoveAcrossMode(lstrip)
     return modes
+
+def get_control( bt, cv ):
+    if bt is not None and ( bt >= 0 and bt < n_modes ):
+        return bt
+    if cv is not None and ( cv >= 0 and cv < n_modes ):
+        return cv
+    return None
 
 def run():
     init_hw()
     modes = init_modes()
     curr_mode_idx = 0
     curr_mode = modes[curr_mode_idx]
+    curr_mode.enter_mode()
 
     while True:
-        read_buttons()
-        read_bt_uart()
-        read_cv_uart()
+        bt_bytes = read_bt_uart()
+        cv_bytes = read_cv_uart()
         
         # decide new mode
+        new_mode_idx = get_control( bt_bytes, cv_bytes )
         new_mode = None
+        if new_mode_idx is not None:
+            new_mode = modes[new_mode_idx]
 
         if new_mode is not None and new_mode != curr_mode:
             curr_mode = new_mode
--- src/modes.py
+++ src/modes.py
@@ -21,3 +21,34 @@ class MoveAcrossMode( Mode ):
         self.strip.set( 0, 0 , 0xff, idx=self.index )
         self.strip.display()
 
+class OffMode( Mode ):
+    def __init__( self, ledstrip ):
+        super().__init__( ledstrip )
+
+    def enter_mode( self ):
+        self.strip.clear()
+
+class FlashMode( Mode ):
+    
+    def __init__( self, ledstrip ):
+        super().__init__( ledstrip )
+        self.mode = 0
+        self.count = 0
+
+    def enter_mode( self ):
+        self.strip.max()
+        self.mode = 1
+        self.count = 0
+
+    def update_mode( self ):
+        if self.count == 10:
+            self.count = 0
+            self.mode = 1 if self.mode == 0 else 0
+
+            if self.mode == 1:
+                self.strip.set( 50, 50, 50, display=True )
+            else:
+                self.strip.set( 0, 0, 0, display=True )
+        
+        self.count = self.count + 1
+
